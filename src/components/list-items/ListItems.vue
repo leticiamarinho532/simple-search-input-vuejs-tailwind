46e96213752079742aaadcf93c00a2ea029a2be2
Leticia <leticia.marinho532@gmail.com>
2023-02-24 20:43:14 -0300

    feat: create search input component + list item component + add heroicons

---
+++ src/components/list-items/ListItems.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<template>
+    <div>
+        List Item Component
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/search-input/SearchInput.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import ListItems from '../list-items/ListItems.vue'
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/solid'
+
+export default {
+    components: {
+        ListItems,
+        MagnifyingGlassIcon
+    }
+}
+</script>
+
+<template>
+    <div>
+        <div class="relative max-w-md border border-slate-200 rounded-md">
+            <input type="text" placeholder="Selecione o país">
+            <MagnifyingGlassIcon class="pointer-events-none h-5 w-5 text-slate-700 absolute top-1/2 transform -translate-y-1/2 right-3"/>
+        </div>
+        <ListItems />
+    </div>
+</template>
+
+<style scoped>
+</style>
